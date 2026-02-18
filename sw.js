b1f16564e58a84e4e936be68388fc20126dd78f8
Sabina <sabinadomagala@gmail.com>
2026-02-18 19:18:31 +0100

    enable offline cache

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "magik-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
